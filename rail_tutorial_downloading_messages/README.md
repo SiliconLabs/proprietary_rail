4c70b62818b914354e55875c827ce02370b5ff75
DonCorleone-silabs <akos.kis@silabs.com>
2024-12-03 10:29:52 +0100

    Adds RAIL Proprietary - Tutorial Downloading Messages example

---
+++ rail_tutorial_downloading_messages/README.md
@@ -0,0 +1,105 @@
+# RAIL Proprietary - Tutorial Downloading Messages #
+
+## Overview ##
+
+This example application demonstrates calibration event handling and downloading
+packets in RAIL.
+
+To get more information about this topic, read our [related
+article](https://community.silabs.com/s/article/rail-tutorial-4-studio5-combining-transmit-and-receive?language=en_US).
+
+> [!IMPORTANT]  
+> This application lacks error handling and other critical features for
+> readability. It is intended solely for educational purposes and is not
+> recommended as a foundation for development.
+
+## SDK version ##
+
+SiSDK 2024.06.00 and above
+
+## Hardware Required ##
+
+EFR32 Series 2
+
+## Connections Required ##
+
+Connect two Development Kits to your PC.
+
+### Tested boards for working with this example ###
+
+| Board ID | Description  |
+| ---------------------- | ------ |
+| BRD4182A | [EFR32xG22 2.4 GHz 6 dBm Radio Board](https://www.silabs.com/development-tools/wireless/slwrb4182a-efr32xg22-wireless-gecko-radio-board?tab=overview) |
+| BRD4204D | [EFR32ZG23 868-915 MHz 14 dBm Radio Board](https://www.silabs.com/development-tools/wireless/xg23-rb4204d-efr32xg23-868-915-mhz-14-dbm-radio-board?tab=overview)|
+| BRD4187C | [EFR32xG24 Wireless 2.4 GHz +20 dBm Radio Board](https://www.silabs.com/development-tools/wireless/xg24-rb4187c-efr32xg24-wireless-gecko-radio-board?tab=overview)|
+| BRD4270B | [EFR32FG25 902-928 MHz +16 dBm Radio Board](https://www.silabs.com/development-tools/wireless/proprietary/fg25-rb4270b-efr32fg25-radio-board?tab=overview) |
+| BRD4401C | [EFR32xG28 868/915 MHz 20 dBm + 2.4 GHz 10 dBm Radio Board](https://www.silabs.com/development-tools/wireless/xg28-rb4401c-efr32xg28-2-4-ghz-ble-and-20-dbm-radio-board?tab=overview) |
+
+## Setup ##
+
+1. Generate Project with [Simplicity Studio
+   v5](https://docs.silabs.com/simplicity-studio-5-users-guide/latest/ss-5-users-guide-about-the-launcher/welcome-and-device-tabs#example-projects-demos-tab)
+   or with [SLC
+   CLI](https://docs.silabs.com/simplicity-studio-5-users-guide/latest/ss-5-users-guide-tools-slc-cli/).
+2. Adjust parameters detailed in the [Configuration](#configuration) section
+   below.
+3. Build and flash the project on two boards.
+
+## How It Works ##
+
+This example application demonstrates calibration event handling and downloading
+packets in RAIL as well as it has packet transmission capabilities. To use this
+application you need two nodes.
+
+The packet sent and received events are indicated with LED0 on the boards. If an
+error occures during the transmission or reception of the packets, LED1 is used
+to indicate this.
+
+If a packet is received by one of the devices, it prints its payload and RSSI (
+received signal strength indicator) value to the terminal.
+
+> `SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH` is a variable used to define
+> the payload length for transmission in the application. Its value should align
+> with the payload length determined by the radio configuration when using a
+> fixed-length configuration. Additionally, it must not exceed the value of
+> `SL_TUTORIAL_DOWNLOADING_MESSAGES_BUFFER_LENGTH`. The length of the Tx buffer
+> should be set according to the radio configuration using this parameter. The
+> `SL_TUTORIAL_DOWNLOADING_MESSAGES_DEFAULT_CHANNEL` parameter determines the
+> index of the channel that is used for packet transmission and reception (see
+> available channels in the radio_settings.radioconf file of the project).
+
+### How to use ###
+
+To initiate a packet transmission, you need to press the PB0 button. Note that
+the default Radio state in this application is Rx state. It goes back to this
+state after each Rx or Tx success/failure, so it is always capable of receiving
+from the other node, and this behavior is symmetric. Additionally, the
+calibration runs before the radio's first use to ensure optimal performance.
+
+### Configuration ###
+
+#### Project Specific Configurations ####
+
+Additional project-specific configurations can be found in the
+`<project_root>/config/sl_rail_tutorial_downloading_messages_config.h` file.
+These configurations are not currently accessible through the UI as component
+settings, so you will need to modify them manually using a text editor or IDE.
+
+#### `SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH` ####
+
+Sets Payload length in bytes.
+
+#### `SL_TUTORIAL_DOWNLOADING_MESSAGES_BUFFER_LENGTH` ####
+
+Sets Tx and Rx Buffer length in bytes.
+
+- [The size must be a power of 2 from 64 to
+  4096](https://docs.silabs.com/rail/latest/rail-api/efr32-main#receive-and-transmit-fifo-buffers)
+
+#### `SL_TUTORIAL_DOWNLOADING_MESSAGES_DEFAULT_CHANNEL` ####
+
+Set the index of the default channel used to transmit and receive.
+
+- Only configured channel index can be used (see available channels in the
+  radio_settings.radioconf file of the project).
+  
---
+++ rail_tutorial_downloading_messages/src/app_process.c
@@ -0,0 +1,175 @@
+/***************************************************************************//**
+ * @file
+ * @brief app_process.c
+ *******************************************************************************
+ * # License
+ * <b>Copyright 2024 Silicon Laboratories Inc. www.silabs.com</b>
+ *******************************************************************************
+ *
+ * SPDX-License-Identifier: Zlib
+ *
+ * The licensor of this software is Silicon Laboratories Inc.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ *
+ *******************************************************************************
+ * # Experimental Quality
+ * This code has not been formally tested and is provided as-is. It is not
+ * suitable for production environments. In addition, this code will not be
+ * maintained and there may be no bug maintenance planned for these resources.
+ * Silicon Labs may update projects from time to time.
+ ******************************************************************************/
+
+// -----------------------------------------------------------------------------
+//                                   Includes
+// -----------------------------------------------------------------------------
+#include "rail.h"
+#include "sl_simple_button_instances.h"
+#include "sl_simple_led_instances.h"
+#include "sl_rail_tutorial_downloading_messages_config.h"
+#include "app_log.h"
+// -----------------------------------------------------------------------------
+//                              Macros and Typedefs
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//                          Static Function Declarations
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//                                Global Variables
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+//                                Static Variables
+// -----------------------------------------------------------------------------
+static uint8_t payload[SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH] =
+{ SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH - 1, 0x01, 0x02, 0x03, 0x04,
+  0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x00 };
+
+static volatile bool send_packet = false;
+
+// This buffer is not the RAIL RX FIFO, but an application buffer.
+static uint8_t rx_buffer[SL_TUTORIAL_DOWNLOADING_MESSAGES_BUFFER_LENGTH];
+// -----------------------------------------------------------------------------
+//                          Public Function Definitions
+// -----------------------------------------------------------------------------
+
+/******************************************************************************
+ * Application state machine, called infinitely
+ *****************************************************************************/
+void app_process_action(RAIL_Handle_t rail_handle)
+{
+  static RAIL_RxPacketHandle_t packet_handle;
+  static RAIL_RxPacketInfo_t packet_info;
+  static RAIL_RxPacketDetails_t packet_details;
+
+  if (send_packet) {
+    send_packet = false;
+    // Increment the last byte of the payload
+    payload[SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH - 1]++;
+    // If the radio configuration's payload settings doesn't match the
+    // SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH, the FIFO might get
+    // corrupt and a FIFO reset may be required in a production quality code
+    // before writing anything to it.
+    uint16_t size = RAIL_WriteTxFifo(rail_handle,
+                                     payload,
+                                     SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH,
+                                     false);
+    if (size != SL_TUTORIAL_DOWNLOADING_MESSAGES_PAYLOAD_LENGTH) {
+      // Turn led1 ON on WriteTxFifo Error
+      sl_led_toggle(&sl_led_led1);
+    }
+    RAIL_Status_t status = RAIL_StartTx(rail_handle,
+                                        SL_TUTORIAL_DOWNLOADING_MESSAGES_DEFAULT_CHANNEL,
+                                        RAIL_TX_OPTIONS_DEFAULT,
+                                        NULL);
+    if (status != RAIL_STATUS_NO_ERROR) {
+      sl_led_toggle(&sl_led_led1);
+    }
+  }
+
+  packet_handle = RAIL_GetRxPacketInfo(rail_handle,
+                                       RAIL_RX_PACKET_HANDLE_OLDEST_COMPLETE,
+                                       &packet_info);
+  if (packet_handle != RAIL_RX_PACKET_HANDLE_INVALID) {
+    RAIL_CopyRxPacket(rx_buffer, &packet_info);
+    RAIL_Status_t status =
+      RAIL_GetRxPacketDetails(rail_handle, packet_handle, &packet_details);
+    if (status != RAIL_STATUS_NO_ERROR) {
+      sl_led_toggle(&sl_led_led1);
+    }
+    status = RAIL_ReleaseRxPacket(rail_handle, packet_handle);
+    if (status != RAIL_STATUS_NO_ERROR) {
+      sl_led_toggle(&sl_led_led1);
+    }
+
+    app_log("RX");
+    for (int i = 0; i < packet_info.packetBytes; i++) {
+      app_log(" 0x%02X", rx_buffer[i]);
+    }
+    app_log("; RSSI=%d dBm\n", packet_details.rssi);
+  }
+}
+
+/******************************************************************************
+ * Button callback, called if a Button event occurs
+ *****************************************************************************/
+void sl_button_on_change(const sl_button_t *handle)
+{
+  if (sl_button_get_state(handle) == SL_SIMPLE_BUTTON_PRESSED) {
+    send_packet = true;
+  }
+}
+
+/******************************************************************************
+ * RAIL callback, called if a RAIL event occurs
+ *****************************************************************************/
+void sl_rail_util_on_event(RAIL_Handle_t rail_handle, RAIL_Events_t events)
+{
+  if (events & RAIL_EVENTS_TX_COMPLETION) {
+    if (events & RAIL_EVENT_TX_PACKET_SENT) {
+      sl_led_toggle(&sl_led_led0);
+    } else {
+      sl_led_toggle(&sl_led_led1); // all other events in
+                                   // RAIL_EVENTS_TX_COMPLETION are errors
+    }
+  }
+  if (events & RAIL_EVENTS_RX_COMPLETION) {
+    if (events & RAIL_EVENT_RX_PACKET_RECEIVED) {
+      if (RAIL_HoldRxPacket(rail_handle) == RAIL_RX_PACKET_HANDLE_INVALID) {
+        sl_led_toggle(&sl_led_led1);
+      }
+      sl_led_toggle(&sl_led_led0);
+    } else {
+      sl_led_toggle(&sl_led_led1); // all other events in
+                                   // RAIL_EVENTS_RX_COMPLETION are errors
+    }
+  }
+  if (events & RAIL_EVENT_CAL_NEEDED) {
+    RAIL_Status_t status = RAIL_Calibrate(rail_handle,
+                                          NULL,
+                                          RAIL_CAL_ALL_PENDING);
+    if (status != RAIL_STATUS_NO_ERROR) {
+      sl_led_toggle(&sl_led_led1);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+//                          Static Function Definitions
+// -----------------------------------------------------------------------------
